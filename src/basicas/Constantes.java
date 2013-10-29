1c0bec18071f90f021a4d3e9aff07a9e498b86b5
Andy <andersonpe@outlook.com.br>
2013-10-28 22:37:49 -0300

    Classe que contém as constantes do programa

---
+++ src/basicas/Constantes.java
@@ -0,0 +1,12 @@
+package entidades;
+
+/**
+ * Classe que contém as constantes do jogo.
+ * @author Andy
+ * @version 2.0
+ */
+public class Constantes {
+
+	public static final String caminhoPadrao = "C:\\LPOO\\galeria\\";
+	
+}
