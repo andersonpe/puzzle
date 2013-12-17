f8d460bf1ef9bef4104c577c53aeac072f214839
Andy <andersonpe@outlook.com.br>
2013-12-17 16:16:27 -0300

    Classe que guarda as constantes do jogo.

---
+++ PuzzleV3/src/constantes/Constantes.java
@@ -0,0 +1,19 @@
+package constantes;
+
+/**
+ * Classe que contém as Constantes do jogo.
+ * @author Anderson Pereira (Andy)
+ * @version 3
+ */
+public class Constantes {
+
+	/** Define a quantidade máxima de rodadas para o primeiro embaralhamento */
+	public static final int NUMERO_MAXIMO_DE_RODADAS = 60;
+	/** Define a quantidade mínina de rodadas para o primeiro embaralhamento. */
+	public static final int NUMERO_MINIMO_DE_RODADAS = 25;
+	/** Define o local onde o arquivo com as opções de imagem estará. */
+	public static final String IMAGENS_LIST = "opcoes_de_imagens\\source.xml";
+	/** Define o local onde os arquivos com as imagens (não divididas) estarão. */
+	public static final String IMAGENS_FOLDER = "galeria_de_imagens\\";
+	
+}
