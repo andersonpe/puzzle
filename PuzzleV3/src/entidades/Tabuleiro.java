d9c3d18f658a66b847ddab4f158bdca7caa88dfb
Andy <andersonpe@outlook.com.br>
2013-12-17 16:39:14 -0300

    Modificação do método que retorna uma determinada peça do tabuleiro.

--- PuzzleV3/src/entidades/Tabuleiro.java
+++ PuzzleV3/src/entidades/Tabuleiro.java
@@ -4,30 +4,60 @@ import java.util.List;
 
 import controladores.ControladorJogo;
 
+/**
+ * Classe que representa um tabuleiro.
+ * @author Anderson Pereira (Andy)
+ * @version 4
+ */
 public class Tabuleiro {
 
+	/* atributo */
+	/** Matriz de peças que representa o tabuleiro.*/
 	private Peca[][] tabuleiro;
-		
+	
+	/**
+	 * Construtor básico da classe que inicializa a matriz de acordo com o
+	 * nível de dificuldade escolhido pelo usuário. 
+	 * */
 	public Tabuleiro() {
 		this(null);
 		int size = ControladorJogo.nivelDificuldadeEscolhido.getNivelEscolhido();
 		tabuleiro = new Peca[size][size];
 	}
 	
+	/**
+	 * 
+	 * @param tabuleiro
+	 */
 	public Tabuleiro(Peca[][] tabuleiro) {
 		this.tabuleiro = tabuleiro;
 	}
 
+	/**
+	 * Método que retorna o tabuleiro completo.
+	 * @return
+	 */
 	public Peca[][] getTabuleiro() {
 		return this.tabuleiro;
 	}
 
+	/**
+	 * Método que modifica o tabuleiro por completo.
+	 * @param tabuleiro
+	 */
 	public void setTabuleiro(Peca[][] tabuleiro) {
 		this.tabuleiro = tabuleiro;
 	}
 	
-	public Peca obterPeca(int i, int j) {
-		return (this.tabuleiro[i][j]);
+	/**
+	 * Método que obtem uma determinada peça do tabuleiro.
+	 * @param coordenada Um coordenada que determinará a localização da peça a ser retornada.
+	 * @return Um objeto do tipo Peca.
+	 */
+	public Peca obterPeca(Coordenada coordenada) {
+		int x = coordenada.getX();
+		int y = coordenada.getY();
+		return (this.tabuleiro[x][y]);
 	}
 	
 	/**
@@ -41,6 +71,11 @@ public class Tabuleiro {
 		return (peca.getImagem() instanceof ImagemEspecial);
 	}
 	
+	/**
+	 * Método que inicializa o tabuleiro.
+	 * @param pecas Um lista de peças que serão adicionada no tabuleiro.
+	 * @see Peca
+	 */
 	public void init(List<Peca> pecas){
 		int i,x,y;
 		Peca peca;
